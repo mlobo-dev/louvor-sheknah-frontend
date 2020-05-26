55117e3ac6dcaf05f7e8e61f02a0e6b20c7ad277
Márcio Lôbo <mlobo.dev@gmail.com>
2020-05-26 09:14:18 -0300

    Login

---
+++ src/main/rotas.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import CadastroUsuario from '../views/cadastroUsuario'
+import Login from '../views/login'
+import { Route, Switch, HashRouter, Redirect } from 'react-router-dom'
+import Home from '../views/home/home';
+import Repertorio from '../views/repertorio/repertorio';
+import { AuthConsumer } from './ProvedorAutenticacao'
+
+function RotaAutenticada({ component: Component, isUsuarioAutenticado, ...props }) {
+    return (
+        <Route {...props} render={(componentProps) => {
+            if (isUsuarioAutenticado) {
+                return (
+                    <Component{...componentProps} />
+                )
+            } else {
+                return (
+                    <Redirect to={{ pathname: '/login', state: { from: componentProps.location } }} />
+                )
+
+            }
+        }} />
+    )
+}
+
+function Rotas(props) {
+    return (
+        <HashRouter>
+            <Switch>
+                <Route path="/login" component={Login} />
+                <Route path="/cadastro-usuarios" component={CadastroUsuario} />
+                <RotaAutenticada isUsuarioAutenticado={props.isUsuarioAutenticado} path="/home" component={Home} />
+                <RotaAutenticada isUsuarioAutenticado={props.isUsuarioAutenticado} path="/repertorios" component={Repertorio} />
+
+            </Switch>
+        </HashRouter>
+    )
+}
+
+export default () => (
+    <AuthConsumer>
+        {(context) => (<Rotas isUsuarioAutenticado={context.isAutenticado} />)}
+    </AuthConsumer>
+)
---
+++ src/views/home/home.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import UsuarioService from '../../app/services/usuarioService'
+import LocalStorageService from '../../app/services/localStorageService'
+import { AuthContext } from '../../main/ProvedorAutenticacao'
+import AtualizacoesTable from './atualizacoesTable'
+class Home extends React.Component {
+
+    state = {
+        saldo: 0
+    }
+
+    constructor() {
+        super();
+        this.usuarioService = new UsuarioService();
+    }
+
+
+    componentDidMount() {
+        // const usuarioLogado = this.context.usuarioAutenticado;
+        const usuarioLogado = LocalStorageService.obterItem('_usuario_logado')
+
+        this.usuarioService
+            .obterSaldoPorIdUsuario(usuarioLogado.id)
+            .then(response => {
+                this.setState({ saldo: response.data })
+            }).catch(error => {
+                console.error(error.response)
+            });
+    }
+
+
+    render() {
+        return (
+            <div className="jumbotron">
+                <h1 className="display-3 text-center">Bem vindo!</h1>
+                <hr className="my-4" />
+                <p><h2 className="text-center">Últimas atualizações</h2></p>
+                <AtualizacoesTable></AtualizacoesTable>
+            </div>
+        )
+    }
+}
+Home.contextType = AuthContext
+export default Home
