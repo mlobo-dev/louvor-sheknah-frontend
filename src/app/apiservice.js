7ad75c2301030a634c715f1eed36e0c13cf94dd4
mlobo-dev <mlobo.dev@gmail.com>
2020-06-27 09:45:57 -0300

    Alterando url serviço

--- src/app/apiservice.js
+++ src/app/apiservice.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const httpClient = axios.create({
-  baseURL: 'http://localhost:8080/'
+ // baseURL: 'https://louvoursheknah-api.herokuapp.com/'
+  baseURL: 'https://localhost:8080'
 })
 
 class ApiService {
